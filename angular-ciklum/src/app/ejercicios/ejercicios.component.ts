a8c56c584b3389f0a5f9088a107ef323779602d9
MarioRR20 <MRRuma2020@uma.es>
2024-03-28 11:33:21 +0100

    Informacion desplegable
    He cambiado el script al .ts para que despliegue informacion del ejercicio y centrado el desplegbale

--- angular-ciklum/src/app/ejercicios/ejercicios.component.ts
+++ angular-ciklum/src/app/ejercicios/ejercicios.component.ts
@@ -8,5 +8,16 @@ import { Component } from '@angular/core';
   styleUrl: './ejercicios.component.css'
 })
 export class EjerciciosComponent {
-
+   mostrarInformacion(nombreEjercicio : String) : void {
+    alert('Información del ejercicio: ' + nombreEjercicio + '\n\n' +
+          '"Nombre": "{{nombreEjercicio.nombre}}",\n' +
+          '"Descripción": "string",\n' +
+          '"Observaciones": "string",\n' +
+          '"Tipo": "string",\n' +
+          '"Músculos Trabajados": "string",\n' +
+          '"Material": "string",\n' +
+          '"Dificultad": "string",\n' +
+          '"Multimedia": ["string"],\n' +
+          '"ID": 0');
+  }
 }
