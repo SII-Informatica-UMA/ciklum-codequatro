edb0224b99fc295fdf4062ffb3d6638f9bbf9fc3
cvv182 <carlosvelascohilario@gmail.com>
2024-04-02 20:43:01 +0200

    Modal para editar ejercicios
    He añadido un modal para poder editar los ejercicios. Además estos guardan los cambios que se le soliciten.

--- angular-ciklum/src/app/ejercicios/ejercicios.component.ts
+++ angular-ciklum/src/app/ejercicios/ejercicios.component.ts
@@ -3,13 +3,16 @@ import { ModalService } from '@developer-partners/ngx-modal-dialog';
 import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Ejercicio } from '../ejercicio';
 import { DetallesEjercicio } from "./detalles-ejercicio/detalles.ejercicio.component";
+import { EditarEjercicio } from "./editar-ejercicio/editar-ejercicio.component";
+import { FormsModule } from '@angular/forms';
+
 
 
 @Component({
   selector: 'app-ejercicios',
   standalone: true,
-  imports: [],
-  templateUrl: './ejercicios.component.html',
+  imports: [FormsModule],
+ templateUrl: './ejercicios.component.html',
   styleUrl: './ejercicios.component.css'
 })
 export class EjercicioComponent{
@@ -186,10 +189,16 @@ export class EjercicioComponent{
         modalRef.componentInstance.ejercicio = ejercicio
       }
 
-      editarEjercicio(index: number){
-        this.editarEjercicioIndex = index
-        this.nuevoEjercicio = {...this.ejercicios[index]}
+      editarEjercicio(ejercicio: any): void{
+       const modalRef = this.modalService.open(EditarEjercicio);
+        modalRef.componentInstance.ejercicio = ejercicio;
+        modalRef.componentInstance.accion = "Editar";
+        modalRef.result.then((r: any) => {
+          let indice = this.ejercicios.findIndex(x => x.nombre == r.nombre);
+          this.ejercicios[indice] = r;
+      }, (reason: any) => {});
       }
+      
 
       actualizarEjercicio(){
         this.ejercicios[this.editarEjercicioIndex] = {...this.nuevoEjercicio};
